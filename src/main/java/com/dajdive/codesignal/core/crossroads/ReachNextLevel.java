3765364dfbd516873523e22a3d6460b0f822d063
dajdive <dajdive@gmail.com>
2021-07-11 11:48:50 -0500

    Added comments

--- src/main/java/com/dajdive/codesignal/core/crossroads/ReachNextLevel.java
+++ src/main/java/com/dajdive/codesignal/core/crossroads/ReachNextLevel.java
@@ -1,5 +1,19 @@
 package main.java.com.dajdive.codesignal.core.crossroads;
 
+/*
+ * You are playing an RPG game. Currently your experience points (XP) total is equal to experience. 
+ * To reach the next level your XP should be at least at threshold. If you kill the monster in 
+ * front of you, you will gain more experience points in the amount of the reward.
+ * 
+ * Given values experience, threshold and reward, check if you reach the next level after 
+ * killing the monster.
+ * 
+ * For experience = 10, threshold = 15, and reward = 5, the output should be
+ * reachNextLevel(experience, threshold, reward) = true;
+ * 
+ * For experience = 10, threshold = 15, and reward = 4, the output should be
+ * reachNextLevel(experience, threshold, reward) = false.
+ */
 public class ReachNextLevel {
 
 	public static boolean reachNextLevel(int experience, int threshold, int reward) {
@@ -10,6 +24,7 @@ public class ReachNextLevel {
 		// Driver class
 
 		System.out.println(reachNextLevel(10, 15, 5));
+		System.out.println(reachNextLevel(10, 15, 4));
 	}
 
 }
